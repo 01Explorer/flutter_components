9bffce646beb7215dad0438529cd6831631255ec
Gabriel Santos <gsantos@imply.com>
2023-10-13 17:15:17 -0300

    feat: implemented the component to handle and drive multiple shimmers

---
+++ lib/components/shimmer_loading/shimmer.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class Shimmer extends StatefulWidget {
+  static ShimmerState? of(BuildContext context) {
+    return context.findAncestorStateOfType<ShimmerState>();
+  }
+
+  const Shimmer({super.key, required this.linearGradient, this.child});
+
+  final LinearGradient linearGradient;
+  final Widget? child;
+
+  @override
+  State<Shimmer> createState() => ShimmerState();
+}
+
+class ShimmerState extends State<Shimmer> with SingleTickerProviderStateMixin {
+  Gradient get gradient => LinearGradient(
+        colors: widget.linearGradient.colors,
+        stops: widget.linearGradient.stops,
+        begin: widget.linearGradient.begin,
+        end: widget.linearGradient.end,
+        transform: _SlidingGradientTransform(_shimmerController.value),
+      );
+
+  Listenable get shimmerChanges => _shimmerController;
+
+  bool get isSized =>
+      (context.findRenderObject() as RenderBox?)?.hasSize ?? false;
+
+  Size get size => (context.findRenderObject() as RenderBox).size;
+
+  Offset getDescendantOffset(
+      {required RenderBox descendant, Offset offset = Offset.zero}) {
+    final shimmerBox = context.findRenderObject() as RenderBox;
+    return descendant.localToGlobal(offset, ancestor: shimmerBox);
+  }
+
+  late AnimationController _shimmerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _shimmerController = AnimationController.unbounded(vsync: this)
+      ..repeat(min: -0.5, max: 1.5, period: const Duration(milliseconds: 1500));
+  }
+
+  @override
+  void dispose() {
+    _shimmerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child ?? const SizedBox();
+  }
+}
+
+class _SlidingGradientTransform extends GradientTransform {
+  final double progress;
+
+  const _SlidingGradientTransform(this.progress);
+
+  @override
+  Matrix4? transform(Rect bounds, {TextDirection? textDirection}) {
+    return Matrix4.translationValues(bounds.width * progress, 0, 0);
+  }
+}
